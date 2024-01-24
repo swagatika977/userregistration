769cccc2a0296dca321de9cc8117916f2628886e
swagatika977 <swagatikadasmohapatra02@gmail.com>
2024-01-25 04:21:50 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from app.forms import *
 from  django.http import HttpResponse,HttpResponseRedirect
 from django.core.mail import send_mail
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 # Create your views here.
 def registration(request):
@@ -47,3 +48,16 @@ def user_login(request):
             return HttpResponse('invalid credentials')
         
     return render(request,'user_login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+@login_required
+def profile_display(request):
+    un=request.session.get('username')
+    UO=User.objects.get(username=un)
+    PO=Profile.objects.get(username=UO)
+    d={'UO':UO,'PO':PO}
+    return render (request,'profile_display.html',d)
